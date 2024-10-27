6cf649d3ef72ec33e263a2945d6eb15652fbb017
TmSalviano <tiagomsalviano@outlook.com>
2024-10-27 10:51:34 -0300

    Add: nav to home page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <nav>
+        <a href="/login">Login Page</a>
+      </nav>
       <p>This is the beginning of my application</p>
     </div>
   );
